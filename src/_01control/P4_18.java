c6ad951c7862ca11e9082173d95226d2072a9f66
Michael Meyer <meyermichael1981@icloud.com>
2016-09-26 18:20:41 -0500

    programming p.4-18 (practice)

---
+++ src/_01control/P4_18.java
@@ -0,0 +1,12 @@
+package _01control;
+
+/**
+ * Created by michaelmeyer on 9/26/16.
+ */
+public class P4_18 {
+
+    public static void main(String[] args) {
+        //my implementation here
+    }
+
+}
